0c73a5d36dc094d3eabc89f07d8a18b598d0f4e4
Nicholas Jillings <nicholas.jillings@mail.bcu.ac.uk>
2016-11-04 15:28:11 +0000

    Minor. Fixed xtract_init_bark

--- jsXtract.min.js
+++ jsXtract.min.js
@@ -2002,9 +2002,11 @@ function xtract_init_pcp(N, fs, f_ref) {
     return M;
 }
 
-function xtract_init_bark(N, sampleRate) {
+function xtract_init_bark(N, sampleRate, bands) {
+    if (typeof bands != "number" || bands < 0 || bands > 26) {
+        bands = 26;
+    }
     var edges = [0, 100, 200, 300, 400, 510, 630, 770, 920, 1080, 1270, 1480, 1720, 2000, 2320, 2700, 3150, 3700, 4400, 5300, 6400, 7700, 9500, 12000, 15500, 20500, 27000];
-    var bands = 26;
     var band_limits = new Int32Array(bands);
     while (bands--) {
         band_limits[bands] = (edges[bands] / sampleRate) * N;
