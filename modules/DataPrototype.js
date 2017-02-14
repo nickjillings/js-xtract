f3374d4ab8e2e183a81ed4660e67d310d0c94f2d
Nicholas Jillings <nicholas.jillings@mail.bcu.ac.uk>
2017-02-14 17:52:54 +0000

    Re-ordered layout. Created singleton jsXtract for DataProto.

--- modules/DataPrototype.js
+++ modules/DataPrototype.js
@@ -1,5 +1,106 @@
 /*globals Float32Array, Float64Array */
 /*globals xtract_init_dct, xtract_init_mfcc, xtract_init_bark */
+
+// Create the Singleton
+var jsXtract = (function () {
+    var dct_map = {
+        parent: this,
+        store: [],
+        createCoefficients: function (N) {
+            var match = this.store.find(function (element) {
+                if (element.N === N) {
+                    return true;
+                }
+                return false;
+            });
+            if (!match) {
+                match = {
+                    N: N,
+                    data: xtract_init_dct(N)
+                };
+                this.store.push(match);
+            }
+            return match.data;
+        }
+    };
+
+    var mfcc_map = {
+        parent: this,
+        store: [],
+        createCoefficients: function (N, nyquist, style, freq_min, freq_max, freq_bands) {
+            var search = {
+                N: N,
+                nyquist: nyquist,
+                style: style,
+                freq_min: freq_min,
+                freq_max: freq_max,
+                freq_bands: freq_bands
+            };
+            var match = this.store.find(function (element) {
+                for (var prop in search) {
+                    if (element[prop] !== search[prop]) {
+                        return false;
+                    }
+                }
+                return true;
+            });
+            if (!match) {
+                match = search;
+                match.data = xtract_init_mfcc(N, nyquist, style, freq_min, freq_max, freq_bands);
+                this.store.push(match);
+            }
+            return match.data;
+        }
+    };
+
+    var bark_map = {
+        parent: this,
+        store: [],
+        createCoefficients: function (N, sampleRate, numBands) {
+            var search = {
+                N: N,
+                sampleRate: sampleRate,
+                numBands: numBands
+            };
+            var match = this.store.find(function (element) {
+                for (var prop in element) {
+                    if (element[prop] !== search[prop]) {
+                        return false;
+                    }
+                }
+                return true;
+            });
+            if (!match) {
+                match = search;
+                match.data = xtract_init_bark(N, sampleRate, numBands);
+                this.store.push(match);
+            }
+            return match.data;
+        }
+    };
+    var pub_obj = {};
+    Object.defineProperties(pub_obj, {
+        "createDctCoefficients": {
+            "value": function (N) {
+                return dct_map.createCoefficients(N);
+            }
+        },
+        "createMfccCoefficients": {
+            "value": function (N, nyquist, style, freq_min, freq_max, freq_bands) {
+                return mfcc_map.createCoefficients(N, nyquist, style, freq_min, freq_max, freq_bands);
+            }
+        },
+        "createBarkCoefficients": {
+            "value": function (N, sampleRate, numBands) {
+                if (typeof numBands !== "number" || numBands < 0 || numBands > 26) {
+                    numBands = 26;
+                }
+                return bark_map.createCoefficients(N, sampleRate, numBands);
+            }
+        }
+    });
+    return pub_obj;
+})();
 var DataProto = function () {
     var _result = {};
     this.clearResult = function () {
@@ -95,95 +196,21 @@ var DataProto = function () {
         this.result.delta.delta = recursiveDelta(this.result.delta, compare.result.delta);
         return this.result.delta.delta;
     };
-
-    var dct_map = {
-        parent: this,
-        store: [],
-        createCoefficients: function (N) {
-            var match = this.store.find(function (element) {
-                if (element.N === this) {
-                    return true;
-                }
-                return false;
-            }, N);
-            if (!match) {
-                match = {
-                    N: N,
-                    data: xtract_init_dct(N)
-                };
-                this.store.push(match);
-            }
-            return match.data;
-        }
-    };
-
-    var mfcc_map = {
-        parent: this,
-        store: [],
-        createCoefficients: function (N, nyquist, style, freq_min, freq_max, freq_bands) {
-            var search = {
-                N: N,
-                nyquist: nyquist,
-                style: style,
-                freq_min: freq_min,
-                freq_max: freq_max,
-                freq_bands: freq_bands
-            };
-            var match = this.store.find(function (element) {
-                for (var prop in this) {
-                    if (element[prop] !== this[prop]) {
-                        return false;
-                    }
-                }
-                return true;
-            }, search);
-            if (!match) {
-                match = search;
-                match.data = xtract_init_mfcc(N, nyquist, style, freq_min, freq_max, freq_bands);
-                this.store.push(match);
-            }
-            return match.data;
-        }
-    };
-
-    var bark_map = {
-        parent: this,
-        store: [],
-        createCoefficients: function (N, sampleRate, numBands) {
-            var search = {
-                N: N,
-                sampleRate: sampleRate,
-                numBands: numBands
-            };
-            var match = this.store.find(function (element) {
-                for (var prop in element) {
-                    if (element[prop] !== this[prop]) {
-                        return false;
-                    }
-                }
-                return true;
-            }, search);
-            if (!match) {
-                match = search;
-                match.data = xtract_init_bark(N, sampleRate, numBands);
-                this.store.push(match);
-            }
-            return match.data;
-        }
-    };
-
-    this.createDctCoefficients = function (N) {
-        return dct_map.createCoefficients(N);
-    };
-
-    this.createMfccCoefficients = function (N, nyquist, style, freq_min, freq_max, freq_bands) {
-        return mfcc_map.createCoefficients(N, nyquist, style, freq_min, freq_max, freq_bands);
-    };
-
-    this.createBarkCoefficients = function (N, sampleRate, numBands) {
-        if (typeof numBands !== "number" || numBands < 0 || numBands > 26) {
-            numBands = 26;
-        }
-        return bark_map.createCoefficients(N, sampleRate, numBands);
-    };
+};
+DataProto.prototype.createDctCoefficients = function (N) {
+    jsXtract.createDctCoefficients(Number(N));
+};
+DataProto.prototype.createMfccCoefficients = function (N, nyquist, style, freq_min, freq_max, freq_bands) {
+    N = Number(N);
+    nyquist = Number(nyquist);
+    freq_min = Number(freq_min);
+    freq_max = Number(freq_max);
+    freq_bands = Number(freq_bands);
+    jsXtract.createMfccCoefficients(N, nyquist, style, freq_min, freq_max, freq_bands);
+};
+DataProto.prototype.createBarkCoefficients = function (N, sampleRate, numBands) {
+    N = Number(N);
+    sampleRate = Number(sampleRate);
+    numBands = Number(numBands);
+    jsXtract.createBarkCoefficients(N, sampleRate, numBands);
 };
