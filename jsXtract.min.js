5ebf643a24526205b19427fdb7218798380c7dc8
Nicholas Jillings <nicholas.jillings@mail.bcu.ac.uk>
2017-02-15 17:45:32 +0000

    Fix bug in xtract_get_number_of_frames

--- jsXtract.min.js
+++ jsXtract.min.js
@@ -142,7 +142,7 @@ function xtract_get_number_of_frames(data, hop_size) {
     if (typeof hop_size !== "number" && hop_size <= 0) {
         throw ("Invalid hop_size. Must be positive integer");
     }
-    return Math.floor(this.length / hop_size);
+    return Math.floor(data.length / hop_size);
 }
 
 function xtract_get_data_frames(data, frame_size, hop_size, copy) {
