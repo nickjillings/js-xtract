5eaa07a3583d63834563cbef9f96ce51f14d773b
Nicholas Jillings <n.g.r.jillings@se14.qmul.ac.uk>
2017-08-15 13:21:56 +0100

    Tidy up xtract_init_mfcc

--- modules/jsXtract.js
+++ modules/jsXtract.js
@@ -2283,6 +2283,45 @@ function xtract_init_dct(N) {
 }
 
 function xtract_init_mfcc(N, nyquist, style, freq_min, freq_max, freq_bands) {
+    function get_fft_peak(freq_max, freq_min, freq_bands, nyquist, style) {
+        var norm = 1,
+            M = N / 2,
+            height, norm_fact, n;
+        var mel_freq_max = 1127 * Math.log(1 + freq_max / 700);
+        var mel_freq_min = 1127 * Math.log(1 + freq_min / 700);
+        var freq_bw_mel = (mel_freq_max - mel_freq_min) / freq_bands;
+
+        var mel_peak = new Float64Array(freq_bands + 2);
+        var lin_peak = new Float64Array(freq_bands + 2);
+        var fft_peak = new Float64Array(freq_bands + 2);
+        var height_norm = new Float64Array(freq_bands);
+        mel_peak[0] = mel_freq_min;
+        lin_peak[0] = freq_min;
+        fft_peak[0] = Math.floor(lin_peak[0] / nyquist * M);
+
+        for (n = 1; n < (freq_bands + 2); ++n) {
+            //roll out peak locations - mel, linear and linear on fft window scale
+            mel_peak[n] = mel_peak[n - 1] + freq_bw_mel;
+            lin_peak[n] = 700 * (Math.exp(mel_peak[n] / 1127) - 1);
+            fft_peak[n] = Math.floor(lin_peak[n] / nyquist * M);
+        }
+
+        for (n = 0; n < freq_bands; n++) {
+            //roll out normalised gain of each peak
+            if (style === "XTRACT_EQUAL_GAIN") {
+                height = 1;
+                norm_fact = norm;
+            } else {
+                height = 2 / (lin_peak[n + 2] - lin_peak[n]);
+                norm_fact = norm / (2 / (lin_peak[2] - lin_peak[0]));
+            }
+            height_norm[n] = height * norm_fact;
+        }
+        return {
+            f: fft_peak,
+            h: height_norm
+        };
+    }
     var mfcc = {
         n_filters: freq_bands,
         filters: []
@@ -2294,39 +2333,12 @@ function xtract_init_mfcc(N, nyquist, style, freq_min, freq_max, freq_bands) {
     if (freq_bands <= 1) {
         return null;
     }
-    var mel_freq_max = 1127 * Math.log(1 + freq_max / 700);
-    var mel_freq_min = 1127 * Math.log(1 + freq_min / 700);
-    var freq_bw_mel = (mel_freq_max - mel_freq_min) / freq_bands;
-
-    var mel_peak = new Float64Array(freq_bands + 2);
-    var lin_peak = new Float64Array(freq_bands + 2);
-    var fft_peak = new Float64Array(freq_bands + 2);
-    var height_norm = new Float64Array(freq_bands);
-    mel_peak[0] = mel_freq_min;
-    lin_peak[0] = freq_min;
-    fft_peak[0] = Math.floor(lin_peak[0] / nyquist * M);
-
-    for (n = 1; n < (freq_bands + 2); ++n) {
-        //roll out peak locations - mel, linear and linear on fft window scale
-        mel_peak[n] = mel_peak[n - 1] + freq_bw_mel;
-        lin_peak[n] = 700 * (Math.exp(mel_peak[n] / 1127) - 1);
-        fft_peak[n] = Math.floor(lin_peak[n] / nyquist * M);
-    }
-
-    for (n = 0; n < freq_bands; n++) {
-        //roll out normalised gain of each peak
-        if (style === "XTRACT_EQUAL_GAIN") {
-            height = 1;
-            norm_fact = norm;
-        } else {
-            height = 2 / (lin_peak[n + 2] - lin_peak[n]);
-            norm_fact = norm / (2 / (lin_peak[2] - lin_peak[0]));
-        }
-        height_norm[n] = height * norm_fact;
-    }
 
     var i = 0,
+        fh = get_fft_peak(freq_max, freq_min, freq_bands, nyquist, style),
         inc;
+    var fft_peak = fh.f,
+        height_norm = fh.h;
     var next_peak;
     for (n = 0; n < freq_bands; n++) {
         // calculate the rise increment
