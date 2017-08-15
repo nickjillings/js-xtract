d65c6a22eefe401d95874b422482ae5b1aa883e1
Nicholas Jillings <n.g.r.jillings@se14.qmul.ac.uk>
2017-08-15 13:47:25 +0100

    Simplify xtract_frame_from_array

--- modules/jsXtract.js
+++ modules/jsXtract.js
@@ -399,24 +399,26 @@ function xtract_array_to_JSON(array) {
 }
 
 function xtract_frame_from_array(src, dst, index, frame_size, hop_size) {
-    if (!xtract_assert_positive_integer(index)) {
-        throw ("xtract_get_frame requires the index to be an integer value");
-    }
-    if (!xtract_assert_positive_integer(frame_size)) {
-        throw ("xtract_get_frame requires the frame_size to be a positive integer");
-    }
     if (hop_size === undefined) {
         hop_size = frame_size;
     }
-    if (!xtract_assert_array(src)) {
-        throw ("Invalid data parameter. Must be item with iterable list");
-    }
-    if (!xtract_assert_array(dst)) {
-        throw ("dst must be an Array-like object equal in length to frame_size");
-    }
-    if (!xtract_assert_positive_integer(hop_size)) {
-        throw ("xtract_get_frame requires the hop_size to be a positive integer");
-    }
+    (function (index, frame_size, src, dst, hop_size) {
+        if (!xtract_assert_positive_integer(index)) {
+            throw ("xtract_get_frame requires the index to be an integer value");
+        }
+        if (!xtract_assert_positive_integer(frame_size)) {
+            throw ("xtract_get_frame requires the frame_size to be a positive integer");
+        }
+        if (!xtract_assert_array(src)) {
+            throw ("Invalid data parameter. Must be item with iterable list");
+        }
+        if (!xtract_assert_array(dst)) {
+            throw ("dst must be an Array-like object equal in length to frame_size");
+        }
+        if (!xtract_assert_positive_integer(hop_size)) {
+            throw ("xtract_get_frame requires the hop_size to be a positive integer");
+        }
+    })(index, frame_size, src, dst, hop_size);
     var K = xtract_get_number_of_frames(src, hop_size);
     if (index >= K) {
         throw ("index number " + index + " out of bounds");
