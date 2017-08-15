76b9917421877a10b76dc2354df8be50a4f32421
Nicholas Jillings <n.g.r.jillings@se14.qmul.ac.uk>
2017-08-15 13:27:08 +0100

    Simplify xtract_onset

--- jsXtract.min.js
+++ jsXtract.min.js
@@ -2006,28 +2006,6 @@ function xtract_yin(array) {
 }
 
 function xtract_onset(timeData, frameSize) {
-    if (!xtract_assert_array(timeData))
-        return 0;
-    if (frameSize === undefined) {
-        throw ("All arguments for xtract_onset must be defined: xtract_onset(timeData, frameSize)");
-    }
-
-    var frames = xtract_get_data_frames(timeData, frameSize, frameSize, false);
-    var N = frames.length;
-    var X = [];
-    var real = new Float64Array(frameSize);
-    var imag = new Float64Array(frameSize);
-    var K = frameSize / 2 + 1;
-    var n;
-    for (var i = 0; i < N; i++) {
-        for (n = 0; n < frameSize; n++) {
-            real[n] = frames[i][n];
-            imag[n] = 0.0;
-        }
-        transform(real, imag);
-        X[i] = xtract_array_interlace([real.subarray(0, K), imag.subarray(0, K)]);
-    }
-
     function angle(real, imag) {
         if (imag === undefined && real.length === 2) {
             return Math.atan2(real[1], real[0]);
@@ -2056,6 +2034,34 @@ function xtract_onset(timeData, frameSize) {
         result[1] = cplx_pair_A[0] * cplx_pair_B[1] + cplx_pair_A[1] * cplx_pair_B[0];
         return result;
     }
+
+    function get_X(frames, frameSize) {
+        var N = frames.length;
+        var X = [];
+        var real = new Float64Array(frameSize);
+        var imag = new Float64Array(frameSize);
+        var K = frameSize / 2 + 1;
+        var n;
+        for (var i = 0; i < N; i++) {
+            for (n = 0; n < frameSize; n++) {
+                real[n] = frames[i][n];
+                imag[n] = 0.0;
+            }
+            transform(real, imag);
+            X[i] = xtract_array_interlace([real.subarray(0, K), imag.subarray(0, K)]);
+        }
+        return X;
+    }
+
+    if (!xtract_assert_array(timeData))
+        return 0;
+    if (frameSize === undefined) {
+        throw ("All arguments for xtract_onset must be defined: xtract_onset(timeData, frameSize)");
+    }
+    var frames = xtract_get_data_frames(timeData, frameSize, frameSize, false);
+    var N = frames.length;
+    var K = frameSize / 2 + 1;
+    var X = get_X(frames, frameSize);
 
     var E = new timeData.constructor(N);
     for (var k = 0; k < K; k++) {
