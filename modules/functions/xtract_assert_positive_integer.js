8ae2224baebf92ebc279bc93d27fc3d93532a610
Nicholas Jillings <nicholas.jillings@mail.bcu.ac.uk>
2021-04-16 09:16:13 +0100

    Fixed export issue in xtract_assert_positive_integer

--- modules/functions/xtract_assert_positive_integer.js
+++ modules/functions/xtract_assert_positive_integer.js
@@ -1,4 +1,4 @@
 /// <reference path="../../typings/functions.d.ts" />
-function xtract_assert_positive_integer(number) {
+export function xtract_assert_positive_integer(number) {
     return (typeof number === "number" && number >= 0 && number === Math.round(number));
 }
